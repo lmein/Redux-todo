517dad2eb88eece379bbf031c711d31e890375cf
lmein <lmeinecke@gmail.com>
2017-05-11 10:09:57 -0500

    Prep for Heroku.

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
 var app = express();
+const PORT = process.env.PORT || 3000;
 
+//this redirects https to http.
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 //static exposes the public directory to the express web server.
 app.use(express.static('public'));
 
 //app.listen starts the server.  two arguements- port & function that is called once server is up.
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000.');
+app.listen(PORT, function () {
+  console.log(`Express server is up on port ${PORT}.``);
 });
