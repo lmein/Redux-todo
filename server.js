51de4f5fd2cf62e8499208bf71be1eb3cb22d49b
lmein <lmeinecke@gmail.com>
2017-05-11 10:32:37 -0500

    Update middleware for localhost.

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 //this redirects https to http.
 app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 //static exposes the public directory to the express web server.
