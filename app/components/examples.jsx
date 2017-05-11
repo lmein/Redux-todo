0e1206a69d27e80b966ffb6c3a5ef1c6cb81540a
lmein <lmeinecke@gmail.com>
2017-05-11 12:11:39 -0500

    Add styles to Examples page.

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -1,4 +1,5 @@
 const React = require('react');
+const {Link} = require('react-router');
 
 // var Examples = React.createClass({
 //   render: function () {
@@ -11,8 +12,16 @@ const React = require('react');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the Examples page.</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Miami'>Miami, FL</Link>
+        </li>
+        <li>
+          <Link to='/?location=Anchorage'>Anchorage, AK</Link>
+        </li>
+      </ol>
     </div>
   );
 };
